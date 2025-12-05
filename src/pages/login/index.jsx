81b2b82a6e6e8fdf8695206d9884bddabe1cd096
kimseyoung <tpdud66770@naver.com>
2025-12-05 13:18:27 +0900

    로그인 페이지 최종 완성

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./login.css"; // CSS 별도 분리
+import "./login.css";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="login-container">
       <div className="login-box">
+        
         <h3 className="login-title">아이디</h3>
         <input
           className="login-input"
@@ -14,11 +17,21 @@ function Login() {
         />
 
         <h3 className="login-title">비밀번호</h3>
-        <input
-          className="login-input"
-          type="password"
-          placeholder="비밀번호를 입력해주세요.."
-        />
+
+        <div className="password-wrapper">
+          <input
+            className="login-input password-input"
+            type={showPassword ? "text" : "password"}
+            placeholder="비밀번호를 입력해주세요."
+          />
+
+          <img
+            src={showPassword ? "/open_eye.png" : "/close_eye.png"}
+            alt="toggle"
+            className="password-icon"
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        </div>
 
         <button className="login-button">로그인</button>
 
