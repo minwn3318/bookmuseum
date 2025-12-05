898c05bb822d67d9d3e1aaf171f7427c3616da05
kimseyoung <tpdud66770@naver.com>
2025-12-05 15:07:24 +0900

    세영 : 통합하고 싶다...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-=======
-// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,12 +6,10 @@ import BookCreatePage from "./pages/register/BookCreatePage.jsx";
 import AiImagePage from "./pages/aiImg/AiImagePage.jsx";
 import BookUpdatePage from "./pages/update/BookUpdatePage.jsx";
 
->>>>>>> 363353d833bbd03871f625b802584a0f813e1606
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Join from "./pages/join";
 
-<<<<<<< HEAD
 
 function App() {
   return (
@@ -31,21 +24,3 @@ function App() {
 }
 
 export default App;
-=======
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/join" element={<Join />} />
-                <Route path="/register" element={<BookCreatePage />} />
-                <Route path="/ai-image" element={<AiImagePage />} />
-                <Route path="/book/update/:bookId" element={<BookUpdatePage />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
-
-export default App;   // ✅ 이게 반드시 필요!
->>>>>>> 363353d833bbd03871f625b802584a0f813e1606
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
->>>>>>> 363353d833bbd03871f625b802584a0f813e1606
