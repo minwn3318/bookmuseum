1e2e11417be9d8fd736e84dd88eb84f88b30b069
kimseyoung <tpdud66770@naver.com>
2025-12-07 17:20:38 +0900

    다온 헤더 + 세영

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+ 
 import BookCreatePage from "./pages/register/BookCreatePage.jsx";
 import AiImagePage from "./pages/aiImg/AiImagePage.jsx";
 import BookUpdatePage from "./pages/update/BookUpdatePage.jsx";
-
+ 
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Join from "./pages/join";
 import Detail from "./pages/detail";
+ 
 import Header from "./components/layout/Header.jsx";
-
-
+ 
+ 
 function App() {
+ 
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    !!localStorage.getItem("token")
+  );
+ 
   return (
     <BrowserRouter>
+ 
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path="/header" element={< Header/>}/>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/join" element={<Join />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/register" element={<BookCreatePage />} />
         <Route path="/ai-image" element={<AiImagePage />} />
         <Route path="/update" element={<BookUpdatePage />} />
       </Routes>
+ 
     </BrowserRouter>
   );
 }
-
+ 
 export default App;
